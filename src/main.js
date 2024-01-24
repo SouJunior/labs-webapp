6b629a20a3f83fe9b036a45570bc24cf555e13a1
Djhonantan Parreira <102682671+djhonantanparreira@users.noreply.github.com>
2024-01-23 22:17:47 -0300

    Add pinia-plugin-persistedstate dependency and implement user store

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
@@ -22,8 +23,10 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 app.use(vuetify)
 
--- src/stores/user.js
+++ src/stores/user.js
@@ -1,4 +1,4 @@
-import { ref, computed } from 'vue'
+import { ref, computed, reactive } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('user', () => {
@@ -6,11 +6,19 @@ export const useUserStore = defineStore('user', () => {
     /** @type { name: string, id: number } */
     const user = ref({ name: '' })
 
-    const getName = computed(() => user.value.name) 
+    const getName = computed(() => user.value.name)
 
     function setName(text) {
-        user.value.name = text 
+        user.value.name = text
     }
 
-    return { user, getName, setName }
-})
+    const state = reactive({
+        users: []
+    })
+
+    function addUser(user) {
+        state.users.push(user)
+    }
+
+    return { user, getName, setName, state, addUser }
+}, { persist: true })
