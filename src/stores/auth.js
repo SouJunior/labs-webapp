824ab9fc951caf0bd415707bb7c465678fde073c
Djhonantan Parreira <102682671+djhonantanparreira@users.noreply.github.com>
2024-02-18 16:32:41 -0300

    Create updateProfile function on store, and update ProfileView.vue

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -9,7 +9,7 @@ export const useAuthStore = defineStore('auth', () => {
 
     const auth = ref({ name: '', email: '', token: '' });
 
-    async function login( user ) {
+    async function login(user) {
         try {
             const response = await axiosInstance.post('/login', user);
             const data = response.data;
@@ -25,14 +25,14 @@ export const useAuthStore = defineStore('auth', () => {
                 user.value = data;
                 auth.value = parseJwt(token);
                 // user.value =  { email: 'teste', password: '', token: token };
-                    // console.log('token :', parseJwt(token));
+                // console.log('token :', parseJwt(token));
 
                 router.push('/onboarding');
             }
 
         } catch (error) {
             console.log('error :', error);
-            if (error.response?.status === 401){
+            if (error.response?.status === 401) {
                 alert(error.response.data)
             }
         }
@@ -52,21 +52,41 @@ export const useAuthStore = defineStore('auth', () => {
         auth.value = { name: '', email: '', token: '' }
     }
 
-    function parseJwt (token) {
+    function parseJwt(token) {
         var base64Url = token.split('.')[1];
         var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
             return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
         }).join(''));
         return JSON.parse(jsonPayload);
     }
 
+    async function updateProfile(attProfile) {
+        try {
+            const response = await axiosInstance.put('/user/' + auth.value.uuid, attProfile);
+            const data = response.data;
 
+            if (data.error) {
+                alert(data.error)
+                return;
+            } else if (data.message) {
+                alert(data.message)
+            }
+            else {
+                alert('Perfil atualizado com sucesso!')
+            }
+        } catch (error) {
+            console.log('error :', error);
+            if (error.response?.status === 401) {
+                alert(error.response.data)
+            }
+        }
+    }
 
-    return { login, logout, auth, getName, $reset}
+    return { login, logout, auth, getName, $reset, updateProfile }
 
 },
-    { 
-        persist: true 
+    {
+        persist: true
     }
 )
