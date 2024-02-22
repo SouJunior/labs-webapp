6951238c3ff6ca091a5991d77156931aae449f9d
Djhonantan Parreira <102682671+djhonantanparreira@users.noreply.github.com>
2024-02-22 17:17:23 -0300

    Refactor user profile form layout and add validation rules

--- src/views/user/ProfileView.vue
+++ src/views/user/ProfileView.vue
@@ -1,38 +1,69 @@
 <template>
   <v-container>
-    <v-row justify="center">
-      <v-col xl="6" xxl="6">
-        <v-sheet class="pa-2">
-    <h1>Dados pessoais</h1>
-    <v-form>
-      <v-text-field label="Nome completo" v-model="profile.name" disabled></v-text-field>
-      <v-text-field label="E-mail" v-model="profile.email" disabled></v-text-field>
-      <v-text-field label="Cidade" v-model="profile.city"></v-text-field>
-      <v-text-field label="Estado" v-model="profile.state"></v-text-field>
-      <v-text-field label="Cargo?" v-model="profile.stack"></v-text-field>
-      <v-text-field label="Perfil do LinkedIn" v-model="profile.linkedin"></v-text-field>
-      <v-text-field label="Perfil no Discord" v-model="profile.discord"></v-text-field>
-      <v-btn color="primary" @click="submitProfile">Atualizar</v-btn>
-      <v-btn color="error" @click="cancelSubmit">Cancelar</v-btn>
-    </v-form>
-        </v-sheet>
+    <v-row align="center" justify="center">
+        <v-col cols="6">
+            <h1 class="text-primary">Dados pessoais:</h1>
+        </v-col>
+    </v-row>
+
+    <v-row align="center" justify="center">
+      <v-col cols="6">
+        <v-form>
+          <v-text-field label="Nome completo *"
+                        variant="outlined"
+                        v-model="profile.name" disabled>
+          </v-text-field>
+          <v-text-field label="E-mail *"
+                        variant="outlined"
+                        v-model="profile.email">
+          </v-text-field>
+          <v-text-field label="Cidade *"
+                        variant="outlined"
+                        v-model="profile.city" :rules="[rules.textOnly]">
+          </v-text-field>
+          <v-text-field label="Estado *"
+                        variant="outlined"
+                        v-model="profile.state" :rules="[rules.textOnly]">
+          </v-text-field>
+          <v-text-field label="Perfil do LinkedIn *"
+                        variant="outlined"
+                        v-model="profile.linkedin">
+          </v-text-field>
+          <v-text-field label="Perfil no Discord *"
+                        variant="outlined"
+                        v-model="profile.discord">
+          </v-text-field>
+          <v-row>
+            <v-col>
+              <v-btn color="primary" @click="submitProfile">Atualizar</v-btn>
+              <v-btn class="ml-3" :to="{ name: 'home' }">cancelar</v-btn>
+            </v-col>
+          </v-row>
+        </v-form>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue';
+import { reactive } from 'vue';
 import { useAuthStore} from '@/stores/auth.js';
 
 const auth = useAuthStore();
 
 const profile = reactive({
   name: auth.auth.name,
-  linkedin: '',
   email: auth.auth.email,
-  password: '12345678'
+  password: '12345678',
+  city: '',
+  state: '',
+  linkedin: '',
+  discord: ''
 });
+
+const rules = {
+  textOnly: (value) => /^[a-zA-Z\sáàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ]*$/.test(value) || 'Apenas texto é permitido.'
+};
 
 const submitProfile = async () => {
   try {
@@ -40,15 +71,5 @@ const submitProfile = async () => {
   } catch (error) {
     console.log(error);
   }
-};
-
-const cancelSubmit = () => {
-  profile.name = auth.auth.name;
-  profile.email = auth.auth.email;
-  profile.city = '';
-  profile.state = '';
-  profile.stack = '';
-  profile.linkedin = '';
-  profile.discord = '';
 };
 </script>
