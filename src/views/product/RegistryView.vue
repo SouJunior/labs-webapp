6aed86ef1fb6e72b3e9e925c9c9e207528b3eede
Djhonantan Parreira <102682671+djhonantanparreira@users.noreply.github.com>
2024-05-10 16:14:25 -0300

    fix: Refactor RegistryView.vue component to improve readability and add text limit to form

--- src/views/product/RegistryView.vue
+++ src/views/product/RegistryView.vue
@@ -1,75 +1,74 @@
 <template>
-  <v-container>
-    <v-row align="center" justify="center">
-      <v-col cols="6">
-        <h1 v-if="route.meta.update == false" class="text-primary">Cadastre seu produto</h1>
-        <h1 v-if="route.meta.update == true" class="text-primary">Atualizar seu produto</h1>
-      </v-col>
-    </v-row>
-
-    <v-row align="center" justify="center">
-      <v-col cols="6">
-        <v-form>
-          <v-text-field
-            v-model="product.name"
-            label="Nome do produto*"
-            variant="outlined"
-          ></v-text-field>
-          <v-textarea
-            v-model="product.description"
-            variant="outlined"
-            label="Descreva seu produto?"
-            hint="Máximo de 280 caracteres"
-            :counter="280"
-            auto-grow
-          ></v-textarea>
-          <v-row align="center" justify="center">
+    <v-container>
+        <v-row align="center" justify="center">
             <v-col cols="6">
-              <v-btn color="primary" @click="submitApplication">Salvar</v-btn>
-              <v-btn class="ml-3" :to="{ name: 'onboarding' }">cancelar</v-btn>
+                <h1 v-if="route.meta.update == false" class="text-primary">Cadastre seu produto</h1>
+                <h1 v-if="route.meta.update == true" class="text-primary">Atualizar seu produto</h1>
             </v-col>
-          </v-row>
-        </v-form>
-      </v-col>
-    </v-row>
-  </v-container>
+        </v-row>
+
+        <v-row  align="center" justify="center">
+            <v-col cols="6">
+                <v-form>
+                    <v-text-field
+v-model="product.name" 
+                                  label="Nome do produto*"
+                                  variant="outlined"></v-text-field>
+                                  <v-textarea
+                                  v-model="product.description"
+                                  variant="outlined"
+                                  label="Descreva seu produto?"
+                                  hint="Máximo de 280 caracteres"
+                                  :counter="280"
+                                  auto-grow
+                                ></v-textarea>
+                    <v-row  align="center" justify="center">
+                        <v-col cols="6">
+                            <v-btn color="primary" @click="submitApplication">Salvar</v-btn>
+                            <v-btn class="ml-3" :to="{ name: 'onboarding' }">cancelar</v-btn>
+                        </v-col>
+                    </v-row>
+                </v-form>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script setup>
-import { reactive } from 'vue'
-import { useProductStore } from '@/stores/product'
-import { useAuthStore } from '@/stores/auth'
-import { useRoute } from 'vue-router'
+import { reactive } from 'vue';
+import { useProductStore } from '@/stores/product';
+import { useAuthStore } from "@/stores/auth";
+import { useRoute } from 'vue-router';
 
-const route = useRoute()
+const route = useRoute();
 
-const productStore = useProductStore()
-const useAuth = useAuthStore()
+const productStore = useProductStore();
+const useAuth = useAuthStore();
 let p = {}
 
-if (route.meta.update == true) {
-  //const index = useAuth.products.map(function(e) { return e.uuid; }).indexOf(route.params.uuid);
-  //p = useAuth.products[index];
-  p = useAuth.getProduct()
+if ( route.meta.update ==  true) {
+    //const index = useAuth.products.map(function(e) { return e.uuid; }).indexOf(route.params.uuid);
+    //p = useAuth.products[index];
+    p = useAuth.getProduct();
 }
 
 const product = reactive({
-  uuid: '',
-  name: '',
-  description: ''
-})
+    uuid: '',
+    name: '',
+    description: '',
+});
 
-if (route.meta.update == true) {
-  product.uuid = route.params.uuid
-  product.name = p.name
-  product.description = p.description
+if ( route.meta.update ==  true) {
+    product.uuid = route.params.uuid
+    product.name = p.name
+    product.description = p.description 
 }
 
 const submitApplication = () => {
-  if (route.meta.update == false) {
-    productStore.create(product)
-  } else {
-    productStore.update(product)
-  }
-}
+    if (route.meta.update == false) {
+        productStore.create(product);
+    } else {
+        productStore.update(product);
+    }
+};
 </script>
